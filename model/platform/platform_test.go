11b83a564902b65c60f7f4a20e23c57dedffc74c
frank <frank123123412345@gmail.com>
2021-01-27 18:51:56 +0400

    add user store and load

--- model/platform/platform_test.go
+++ model/platform/platform_test.go
@@ -1,6 +1,7 @@
 package platform
 
 import (
+	redisGo "github.com/gomodule/redigo/redis"
 	"jarvis/base/database"
 	"log"
 	"testing"
@@ -9,7 +10,7 @@ import (
 
 func TestQueryPlatformInfoByID(t *testing.T) {
 	// 初始化 Redis
-	database.InitializeRedis(time.Minute*time.Duration(5), 10, 5000, "localhost", 6379, "frank123")
+	database.InitializeRedis(time.Minute*time.Duration(5), 10, 5000, "localhost", 8000, "frank123")
 
 	//p, err := QueryPlatformInfoByID("1")
 	//if err != nil {
@@ -27,13 +28,22 @@ func TestQueryPlatformInfoByID(t *testing.T) {
 	//
 	//log.Printf("%+v", platforms)
 
-	i := 0
-	for i < 5000 {
-		i++
-		go func() {
-			log.Println(HExistsPlatformByID("2"))
-		}()
-	}
+	//i := 0
+	//for i < 5000 {
+	//	i++
+	//	go func() {
+	//		log.Println(HExistsPlatformByID("2"))
+	//	}()
+	//}
+	//
+	//time.Sleep(time.Duration(3) * time.Second)
 
-	time.Sleep(time.Duration(3) * time.Second)
+	conn, err := database.GetRedisConn()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer conn.Close()
+
+	log.Println(redisGo.String(conn.Do("set", "name", "frank")))
 }
--- model/user/user.go
+++ model/user/user.go
@@ -1,7 +1,12 @@
 package user
 
+import (
+	"context"
+	"jarvis/base/database"
+)
+
 type (
-	// 账号信息
+	// 账号信息 `dynamic_account`
 	Account struct {
 		ID          int    `json:"id"`       // 账号 id
 		Token       string `json:"token"`    // 令牌(唯一)
@@ -11,9 +16,24 @@ type (
 		Platform    int    `json:"platform"` // 所属平台
 	}
 
+	// 用户信息 `dynamic_userInfo`
+	Info struct {
+		ID                int    `json:"id"`                   // 用户 id
+		AccountToken      string `json:"account_token"`        // 账号唯一标识
+		Name              string `json:"name"`                 // 用户名
+		Age               int    `json:"age"`                  // 用户年龄
+		Sex               bool   `json:"sex"`                  // 用户性别
+		HeadImage         int    `json:"head_image"`           // 用户头像序号
+		Vip               int    `json:"vip"`                  // 用户 vip 等级
+		GameBgMusicVolume int    `json:"game_bg_music_volume"` // 背景音乐音量
+		GameEffectVolume  int    `json:"game_effect_volume"`   // 音效音量
+		AccountBalance    int64  `json:"account_balance"`      // 账户余额(单位:分)
+	}
+
 	// 用户
 	User struct {
 		Account Account `json:"account"` // 账号信息
+		Info    Info    `json:"info"`    // 用户信息
 	}
 )
 
@@ -21,5 +41,112 @@ type (
 func FreshUser() User {
 	return User{
 		Account: Account{},
+		Info:    Info{},
 	}
+}
+
+// 通过账号密码加载用户信息
+func (u *User) LoadInfoByAccountAndPassword(account, password string) error {
+	if err := u.Account.loadByAccountAndPassword(account, password); err != nil {
+		return err
+	}
+
+	if err := u.Info.loadByAccountToken(u.Account.Token); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 存储信息
+func (u *User) Store() error {
+	if err := u.Account.Store(); err != nil {
+		return err
+	}
+
+	if err := u.Info.Store(u.Account.Token); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 通过账号密码加载账号信息
+func (a *Account) loadByAccountAndPassword(account, password string) error {
+	// 获取 MySQL 连接
+	mysqlConn, err := database.GetMySQLConn()
+	if err != nil {
+		return err
+	}
+	defer mysqlConn.Close()
+
+	row := mysqlConn.QueryRowContext(context.Background(), "select * from `jarvis`.`dynamic_account` where account = ? and password = ?", account, password)
+	if err := row.Scan(&a.ID, &a.Token, &a.Account, &a.Password, &a.AccountType, &a.Platform); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 存储信息
+func (a *Account) Store() error {
+	// 获取 MySQL 连接
+	mysqlConn, err := database.GetMySQLConn()
+	if err != nil {
+		return err
+	}
+	defer mysqlConn.Close()
+
+	_, err = mysqlConn.ExecContext(context.Background(), "insert into `jarvis`.`dynamic_account`(token, account, password, type, platform) values (?,?,?,?,?)",
+		a.Token,
+		a.Account,
+		a.Password,
+		a.AccountType,
+		a.Platform)
+
+	return err
+}
+
+// 通过 token 加载账号信息
+func (i *Info) loadByAccountToken(token string) error {
+	// 获取 MySQL 连接
+	mysqlConn, err := database.GetMySQLConn()
+	if err != nil {
+		return err
+	}
+	defer mysqlConn.Close()
+
+	row := mysqlConn.QueryRowContext(context.Background(), "select id, account_token, name, age, sex, head_image, vip, game_bg_music_volume, game_effect_volume, account_balance from `jarvis`.`dynamic_userInfo` where account_token = ?", token)
+	if err := row.Scan(
+		&i.ID,
+		&i.AccountToken,
+		&i.Name,
+		&i.Age,
+		&i.Sex,
+		&i.HeadImage,
+		&i.Vip,
+		&i.GameBgMusicVolume,
+		&i.GameEffectVolume,
+		&i.AccountBalance,
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 通过 token 加载账号信息
+func (i *Info) Store(token string) error {
+	// 获取 MySQL 连接
+	mysqlConn, err := database.GetMySQLConn()
+	if err != nil {
+		return err
+	}
+	defer mysqlConn.Close()
+
+	_, err = mysqlConn.ExecContext(context.Background(), "insert into `jarvis`.`dynamic_userInfo`(account_token, name) values (?,?)",
+		token,
+		i.Name)
+
+	return err
 }
