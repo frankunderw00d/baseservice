61ac5b93bd94ba5a25847e784b6750862b29210a
frank <frank123123412345@gmail.com>
2021-01-29 18:36:00 +0400

    update

---
+++ baseservice.go
@@ -0,0 +1,16 @@
+// 提供公用服务支持
+// baseservice/
+// ├── base
+// │   ├── base.go
+// │   └── basic
+// │       ├── basic.go
+// │       └── secretKey.go
+// ├── model
+// │   ├── platform ： 提供平台模型及相关函数
+// │   └── user ： 提供用户模型及相关函数
+
+package baseservice
+
+const (
+	Version = "1.0.0"
+)
